7a7ca6eda833842da899e661c879e58386df3fe4
Maxime Soulie <20998968+MaximeSoulie@users.noreply.github.com>
2020-10-18 01:07:43 -0400

    add app python

---
+++ 2-Build_and_Deploy/upload_docker.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+dockerpath=msoulie/p5-microservice-k8s:latest
+
+# Step 2:  
+# Authenticate & tag
+echo "Docker ID and Image: $dockerpath"
+
+# Step 3:
+# Push image to a docker repository
+docker login -u msoulie
+docker push $dockerpath 
